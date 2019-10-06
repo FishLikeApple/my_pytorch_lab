f21c6618079861d6d3a36b4adae02f6f7c537adb
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2019-10-06 20:43:28 +0800

    Update train.py

--- classification_with_res100/train.py
+++ classification_with_res100/train.py
@@ -1,1 +1,14 @@
-import troch
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import os
+
+#define the model
+model = models.resnet101(pretrained=True)
+print(model.classifier)
+num_ftrs = model.classifier[6].in_features
+model.classifier[6] = nn.Linear(num_ftrs, 2)

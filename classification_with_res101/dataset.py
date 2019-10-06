c1ab753448313cf64026d2ba8bb6bbc86330932c
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2019-10-07 00:00:58 +0800

    Create dataset.py

---
+++ classification_with_res101/dataset.py
@@ -0,0 +1,146 @@
+import os 
+import cv2
+import numpy as np
+import pandas as pd 
+import torch
+from torch.utils.data import Dataset
+from torchvision import transforms
+from PIL import Image
+from sklearn.model_selection import train_test_split 
+from albumentations import (
+    PadIfNeeded,
+    HorizontalFlip,
+    VerticalFlip,    
+    CenterCrop,    
+    Crop,
+    Compose,
+    Transpose,
+    RandomRotate90,
+    ElasticTransform,
+    GridDistortion, 
+    OpticalDistortion,
+    RandomSizedCrop,
+    OneOf,
+    CLAHE,
+    RandomBrightnessContrast,    
+    RandomGamma,
+    Normalize,
+    Resize
+)
+from albumentations.torch import ToTensor
+from torchvision import transforms 
+import random 
+from imgaug import augmenters as iaa
+
+def get_transforms(phase):
+    original_height = 256
+    original_width = 1600
+    list_transforms = []
+    if phase == "train":
+        list_transforms.extend(
+            [
+            OneOf([
+                RandomSizedCrop(min_max_height=(50, 101), height=original_height, width=original_width, p=0.5),
+                PadIfNeeded(min_height=original_height, min_width=original_width, p=0.5)], p=1),    
+                VerticalFlip(p=0.5),              
+                # RandomRotate90(p=0.5),
+                OneOf([
+                    ElasticTransform(p=0.5, alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03),
+                    GridDistortion(p=0.5),
+                    OpticalDistortion(p=1, distort_limit=2, shift_limit=0.5)                  
+                ], p=0.8),
+                CLAHE(p=0.8),
+                RandomBrightnessContrast(p=0.8),    
+                RandomGamma(p=0.8),
+            ]
+        )
+    list_transforms.extend(
+        [
+            Resize(height=128, width=800,  interpolation=cv2.INTER_NEAREST),
+            Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), p=1),
+            ToTensor(),
+        ]
+    )
+    list_trfms = Compose(list_transforms)
+    return list_trfms
+    
+class SteelDataset(Dataset):
+    def __init__(self, root_dataset, list_data, phase, mode='cls'):
+        super(SteelDataset, self).__init__()
+        self.mode = mode
+        self.root_dataset = root_dataset
+        self.df = self.__read_file__(list_data=list_data)
+        self.transforms = get_transforms(phase)
+    
+    def __read_file__(self, list_data):
+        df = pd.read_csv(os.path.join(list_data))
+        df['ImageId'], df['ClassId'] = zip(*df['ImageId_ClassId'].str.split('_'))
+        df['ClassId'] = df['ClassId'].astype(int)
+        df = df.pivot(index='ImageId',columns='ClassId',values='EncodedPixels')
+        df['defects'] = df.count(axis=1)
+        return df
+    def make_cls(self, row_id, df):
+        fname = df.iloc[row_id].name
+        labels = df.iloc[row_id][:4]
+        
+          if len(labels) == 0:
+              cls = 0
+          else:
+              cls = 1
+        return fname, masks
+    def __getitem__(self, index):
+        image_id, mask = self.make_mask(index, self.df)
+        if 1 not in mask:
+            cls = 0
+        else:
+            cls = 1
+        image_path = os.path.join(self.root_dataset, image_id)
+        img = cv2.imread(image_path)
+        augmented = self.transforms(image=img)
+        img = augmented['image']
+        if self.mode == 'cls':
+            mask = mask[0].permute(2, 0, 1) # 1x4x256x1600
+            mask = (mask.view(4, -1).sum(1)>0)
+            mask = mask.float()
+        else:
+            mask = mask*torch.tensor([1, 2, 3, 4], dtype=torch.float32)
+            mask, _ = torch.max(mask, -1) 
+            mask = mask.long()
+        return img, cls
+
+    def __len__(self):
+        return len(self.df)
+
+def image_to_input(image,rbg_mean,rbg_std):#, rbg_mean=[0,0,0], rbg_std=[1,1,1]):
+    input = image.astype(np.float32)
+    input = input[...,::-1]/255
+    input = input.transpose(0,3,1,2)
+    input[:,0] = (input[:,0]-rbg_mean[0])/rbg_std[0]
+    input[:,1] = (input[:,1]-rbg_mean[1])/rbg_std[1]
+    input[:,2] = (input[:,2]-rbg_mean[2])/rbg_std[2]
+    return input
+
+def null_collate(batch):
+    batch_size = len(batch)
+    input = []
+    truth_mask  = []
+
+    for b in range(batch_size):
+        input.append(batch[b][0])
+
+        mask  = batch[b][1]
+        num_class,H,W = mask.shape
+        print(mask.shape)
+        mask = mask.permute(1, 2, 0)*torch.tensor([1, 2, 3, 4])
+
+        mask = mask.view(-1, 4)
+        mask = mask.reshape(-1,4) 
+        mask = mask.max(-1).reshape(1,H,W) 
+        truth_mask.append(mask)
+
+    input = np.stack(input)
+    # input = image_to_input(input, [0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    input = torch.from_numpy(input).float()
+    truth_mask = np.stack(truth_mask)
+    truth_mask = torch.from_numpy(truth_mask).long()
+    return input, truth_mask

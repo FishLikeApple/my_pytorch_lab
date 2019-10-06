22a0bd444609b3daa5d9f08c875f118d9c8df58a
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2019-10-06 23:47:37 +0800

    Update train.py

--- classification_with_res101/train.py
+++ classification_with_res101/train.py
@@ -7,6 +7,9 @@ import torchvision
 from torchvision import datasets, models, transforms
 import os
 
+#define some hyperparameters
+num_classes = 2
+
 #define the model
 model = models.resnet101(pretrained=True)
-model.fc=nn.Linear(model.fc.in_features, 2)
+model.fc=nn.Linear(model.fc.in_features, num_classes)

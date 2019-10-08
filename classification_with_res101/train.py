468f929be8f1bcdf3e6575fbb300dfca6db39f9f
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2019-10-08 21:38:22 +0800

    Update train.py

--- classification_with_res101/train.py
+++ classification_with_res101/train.py
@@ -5,7 +5,7 @@ from torch.optim import lr_scheduler
 from torch.utils.data import DataLoader 
 import numpy as np
 import torchvision
-from torchvision import models, transforms
+from torchvision import transforms
 import os
 from dataset import SteelDataset
 import time
@@ -24,22 +24,41 @@ parser.add_argument('--epoch_start', default=0, type=int)
 parser.add_argument('--num_epoch', default=200, type=int)
 parser.add_argument('--num_class', default=5, type=int)
 parser.add_argument('--num_workers', default=1, type=int)
+parser.add_argument('--n_folds', default=0, type=int)
 parser.add_argument('--clearing_steps', default=12, type=int)
 args = parser.parse_args()
 
 #define some hyperparameters
 num_classes = 2
 
-#define the model and so on
-model = models.resnet101(pretrained=True)
-model.fc=nn.Linear(model.fc.in_features, num_classes)
-model = model.cuda()
-train_dataset = SteelDataset(root_dataset = args.train_dataset, list_data = args.list_train, phase='train')
-train_loader = DataLoader(train_dataset, batch_size = args.batch_size, shuffle=True, num_workers=args.num_workers)
+#define the models and datasets
+if n_folds <= 0:
+    train_dataset = SteelDataset(root_dataset = args.train_dataset, list_data = args.list_train, phase='train')
+    train_loaders = [DataLoader(train_dataset, batch_size = args.batch_size, shuffle=True, num_workers=args.num_workers)]
+    valid_dataset = SteelDataset(root_dataset = args.train_dataset, list_data = args.list_train, phase='valid')
+    valid_loaders = [DataLoader(valid_dataset, batch_size = args.batch_size, shuffle=True, num_workers=args.num_workers)]
+    models = [torchvision.models.resnet101(pretrained=True)]
+    models[-1].fc=nn.Linear(model.fc.in_features, num_classes)
+    models[-1] = models[-1].cuda()
+else:
+    train_loaders = []
+    vaild_loaders = []
+    models = []
+    for i in range(n_folds):
+        train_dataset = SteelDataset(root_dataset = args.train_dataset, list_data = args.list_train, 
+                                     phase='train', fold_i=i, n_folds=n_folds)
+        train_loaders.append(DataLoader(train_dataset, batch_size = args.batch_size, shuffle=True, num_workers=args.num_workers))
+        valid_dataset = SteelDataset(root_dataset = args.train_dataset, list_data = args.list_train, 
+                                     phase='valid', fold_i=i, n_folds=n_folds)
+        valid_loaders.append(DataLoader(valid_dataset, batch_size = args.batch_size, shuffle=True, num_workers=args.num_workers))
+        models.append(torchvision.models.resnet101(pretrained=True))
+        models[-1].fc=nn.Linear(model.fc.in_features, num_classes)
+        models[-1] = models[-1].cuda()
+        
 optimizer = optim.Adam(model.parameters(), lr=args.lr)
 loss_fn = nn.CrossEntropyLoss()
 
-def train(data_loader):
+def train(data_loader, model):
     model.train()
     total_loss = 0
     accumulation_steps = 32 // args.batch_size
@@ -62,15 +81,41 @@ def train(data_loader):
         torch.cuda.empty_cache()
             
     return total_loss/len(data_loader)
+
+def valid(data_loader, model):
+    model.eval()
+    num_correct = 0
+    accumulation_steps = 32 // args.batch_size
+    optimizer.zero_grad()
+    for idx, (img, cls) in enumerate(tqdm(data_loader)):
+        img = img.cuda()
+        cls = cls.cuda()
+        outputs = model(img)
+        
+        preds = outputs.argmax(dim=1)
+        num_correct += torch.eq(pred, cls).sum().float().item()
+        
+        # delete caches
+        del img, cls, outputs, preds
+        torch.cuda.empty_cache()
+            
+    return num_correct/len(data_loader)
   
 for epoch in range(args.epoch_start, args.epoch_start+args.num_epoch):
     start_time = time.time()
-    loss_train = train(train_loader)
-    print('[TRAIN] Epoch: {}| Loss: {}| Time: {}'.format(epoch, loss_train, time.time()-start_time))
-    state = {
-    "status": 'not used',
-    "epoch": epoch,
-    "arch": 'res101',
-    "state_dict": model.state_dict()
-    }
-    torch.save(state, '{}{}_checkpoint_{}.pth'.format(args.new_checkpoint_path, 'res101', epoch))
+    corrections_train = []
+    
+    for i in range(len(train_loaders)):
+        loss_train = train(train_loaders[i], models[i])
+        corrections_train.append(valid(train_loaders[i], models[i]))
+        print('[TRAIN] Epoch: {}| Loss: {}| Time: {}'.format(epoch, loss_train, time.time()-start_time))
+        state = {
+        "status": 'not used',
+        "epoch": epoch,
+        "arch": 'res101_' + str(i),
+        "state_dict": models[i].state_dict()
+        }
+        torch.save(state, '{}{}_checkpoint_{}.pth'.format(args.new_checkpoint_path, 'res101_' + str(i), epoch))
+        
+    print('correction:'+str(sum(corrections_train)/len(corrections_train)))
+     

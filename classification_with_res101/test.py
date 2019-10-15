eeec9db17093ed16c659734b6061cca420e2b770
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2019-10-15 17:18:12 +0800

    Update test.py

--- classification_with_res101/test.py
+++ classification_with_res101/test.py
@@ -16,6 +16,7 @@ import argparse
 parser = argparse.ArgumentParser(description='Semantic Segmentation')
 parser.add_argument('--test_dataset', default='./data/test_images', type=str, help='config file path')
 parser.add_argument('--list_test', default='./data/test.csv', type=str)
+parser.add_argument('--batch_size', default=None, type=int)
 parser.add_argument('--checkpoint', default=None, type=str)
 parser.add_argument('--num_class', default=2, type=int)
 parser.add_argument('--num_workers', default=1, type=int)
@@ -27,94 +28,39 @@ args = parser.parse_args()
 num_classes = args.num_class
 
 #define the models, optimizers and datasets
+test_dataset = SteelDataset(root_dataset = args.test_dataset, list_data = args.list_test, phase='test')
+test_loader = DataLoader(test_dataset, batch_size = args.batch_size, shuffle=False, num_workers=args.num_workers)
 if args.n_folds <= 0:
-    train_dataset = SteelDataset(root_dataset = args.train_dataset, list_data = args.list_train, phase='train')
-    train_loaders = [DataLoader(train_dataset, batch_size = args.batch_size, shuffle=True, num_workers=args.num_workers)]
     #valid_dataset = SteelDataset(root_dataset = args.train_dataset, list_data = args.list_train, phase='valid')
-    valid_loaders = [] #[DataLoader(valid_dataset, batch_size = args.batch_size, shuffle=True, num_workers=args.num_workers)]
     models = [torchvision.models.resnet101(pretrained=True)]
     models[-1].fc=nn.Linear(models[-1].fc.in_features, num_classes)
     models[-1] = models[-1].cuda()
-    optimizers = [optim.Adam(models[-1].parameters(), lr=args.lr)]
+    if args.checkpoint != None:
+        models[-1].load_state_dict(torch.load(args.checkpoint+'_'+str(0))['state_dict']) 
 else:
-    train_loaders = []
-    valid_loaders = []
     models = []
-    optimizers = []
-    for i in range(args.n_folds):
-        train_dataset = SteelDataset(root_dataset = args.train_dataset, list_data = args.list_train, 
-                                     phase='train', fold_i=i, n_folds=args.n_folds)
-        train_loaders.append(DataLoader(train_dataset, batch_size = args.batch_size, shuffle=True, num_workers=args.num_workers))
-        valid_dataset = SteelDataset(root_dataset = args.train_dataset, list_data = args.list_train, 
-                                     phase='valid', fold_i=i, n_folds=args.n_folds)
-        valid_loaders.append(DataLoader(valid_dataset, batch_size = args.batch_size, shuffle=True, num_workers=args.num_workers))
+    for i in range(args.n_folds): 
         models.append(torchvision.models.resnet101(pretrained=True))
         models[-1].fc=nn.Linear(models[-1].fc.in_features, num_classes)
         models[-1] = models[-1].cuda()
-        optimizers.append(optim.Adam(models[-1].parameters(), lr=args.lr))
-        
-loss_fn = nn.CrossEntropyLoss()
-
-def train(data_loader, model, optimizer):
-    model.train()
-    total_loss = 0
-    accumulation_steps = 32 // args.batch_size
-    optimizer.zero_grad()
-    for idx, (img, cls) in enumerate(tqdm(data_loader)):
-        img = img.cuda()
-        cls = cls.cuda()
-        outputs = model(img)
-        loss = loss_fn(outputs, cls)
-        (loss/accumulation_steps).backward()
-        clipping_value = 1.0
-        nn.utils.clip_grad_norm_(model.parameters(), clipping_value)
-        if (idx + 1 ) % accumulation_steps == 0:
-            optimizer.step()
-            optimizer.zero_grad()
-            with open(args.new_checkpoint_path+'logs.txt', 'a') as f:
-                print('idx:'+str(idx)+'  last_loss:'+str(loss), f)
-        total_loss += loss.item()
-        
-        # delete caches
-        del img, cls, outputs, loss
-        torch.cuda.empty_cache()
-            
-    return total_loss/len(data_loader)
-
-def valid(data_loader, model):
-    model.eval()
-    num_correct = 0
-    accumulation_steps = 32 // args.batch_size
-    for idx, (img, cls) in enumerate(tqdm(data_loader)):
-        img = img.cuda()
-        cls = cls.cuda()
-        outputs = model(img)
-        
-        preds = outputs.argmax(dim=1)
-        num_correct += torch.eq(preds, cls).sum().float().item()
-        
-        # delete caches
-        del img, cls, outputs, preds
-        torch.cuda.empty_cache()
-            
-    return num_correct/len(data_loader)
-  
-for epoch in range(args.epoch_start, args.epoch_start+args.num_epoch):
-    start_time = time.time()
-    corrections_train = []
+        if args.checkpoint != None:
+            models[-1].load_state_dict(torch.load(args.checkpoint+'_'+str(i))['state_dict'])
+               
+submission = pd.read_csv(args.list_test)    
     
-    for i in range(len(train_loaders)):
-        loss_train = train(train_loaders[i], models[i], optimizers[i])
-        if len(valid_loaders) > 0:
-            corrections_train.append(valid(valid_loaders[i], models[i]))
-        print('[TRAIN] Epoch: {}| Fold: {}| Loss: {}| Time: {}'.format(epoch, i, loss_train, time.time()-start_time))
-        state = {
-        "status": 'not used',
-        "epoch": epoch,
-        "arch": 'res101_' + str(i),
-        "state_dict": models[i].state_dict()
-        }
-        torch.save(state, '{}{}_checkpoint_{}.pth'.format(args.new_checkpoint_path, 'res101_' + str(i), epoch))
-    
-    if len(valid_loaders) > 0:
-        print('correction:'+str(sum(corrections_train)/len(corrections_train)))
+def test(data_loader, models):
+    for model in models: 
+        model.eval()
+    for img, segm, img_id in tqdm(data_loader):
+        img = img.cuda()
+        
+        output = models[-1](img).cpu().detach().numpy()
+        for model in models[1:]:
+            output += model(img).cpu().detach().numpy()
+        print(output)
+        a = 1/0
+        submission.loc[submission['ImageId_ClassId']==img_id[0]+'_'+str(type), 'EncodedPixels'] = rle
+            
+    submission.to_csv(args.submission)
+                
+test(test_loader, models)     

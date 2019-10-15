13266d8cc6ff981c1cbe0acf05210d4ed32f320d
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2019-10-15 15:17:38 +0800

    Update test.py

--- classification_with_res101/test.py
+++ classification_with_res101/test.py
@@ -1,88 +1,120 @@
-import os 
-import argparse 
-import torch 
-from dataset import SteelDataset 
-from torch.utils.data import DataLoader 
-import torch.nn as nn 
-from optimizers import RAdam 
-from utils import Metric
-from tqdm import tqdm 
+import torch
+import torch.nn as nn
 import torch.optim as optim
-from models.model import Model
-import time 
-from torch.optim.lr_scheduler import ReduceLROnPlateau
-import torchvision
-from models.loss import Criterion
-from datasets.dataset import null_collate
+from torch.optim import lr_scheduler
+from torch.utils.data import DataLoader 
 import numpy as np
-import pandas as pd
+import torchvision
+from torchvision import transforms
+import os
+from dataset import SteelDataset
+import time
+from tqdm import tqdm
 
+#get input args 
+import argparse
 parser = argparse.ArgumentParser(description='Semantic Segmentation')
 parser.add_argument('--test_dataset', default='./data/test_images', type=str, help='config file path')
-parser.add_argument('--checkpoint', default='./checkpoint.pth', type=str, help='config file path')
 parser.add_argument('--list_test', default='./data/test.csv', type=str)
-parser.add_argument('--submission', default='./submission.csv', type=str)
-parser.add_argument('--num_class', default=5, type=int)
+parser.add_argument('--checkpoint', default=None, type=str)
+parser.add_argument('--num_class', default=2, type=int)
 parser.add_argument('--num_workers', default=1, type=int)
-parser.add_argument('--encoder', default="resnet34", type=str)
-parser.add_argument('--decoder', default="hrnet", type=str)  
-parser.add_argument('--mode', default='non-cls', type=str)
+parser.add_argument('--n_folds', default=0, type=int)
+parser.add_argument('--clearing_steps', default=12, type=int)
 args = parser.parse_args()
 
-test_dataset = SteelDataset(root_dataset = args.test_dataset, list_data = args.list_test, phase='test', mode=args.mode)
+#define some hyperparameters
+num_classes = args.num_class
 
-model = Model(num_class=args.num_class, encoder = args.encoder, decoder = args.decoder, mode=args.mode)
-model = model.cuda()
-model.load_state_dict(torch.load(args.checkpoint)['state_dict'])
-model.eval()
-criterion = Criterion(mode=args.mode)
+#define the models, optimizers and datasets
+if args.n_folds <= 0:
+    train_dataset = SteelDataset(root_dataset = args.train_dataset, list_data = args.list_train, phase='train')
+    train_loaders = [DataLoader(train_dataset, batch_size = args.batch_size, shuffle=True, num_workers=args.num_workers)]
+    #valid_dataset = SteelDataset(root_dataset = args.train_dataset, list_data = args.list_train, phase='valid')
+    valid_loaders = [] #[DataLoader(valid_dataset, batch_size = args.batch_size, shuffle=True, num_workers=args.num_workers)]
+    models = [torchvision.models.resnet101(pretrained=True)]
+    models[-1].fc=nn.Linear(models[-1].fc.in_features, num_classes)
+    models[-1] = models[-1].cuda()
+    optimizers = [optim.Adam(models[-1].parameters(), lr=args.lr)]
+else:
+    train_loaders = []
+    valid_loaders = []
+    models = []
+    optimizers = []
+    for i in range(args.n_folds):
+        train_dataset = SteelDataset(root_dataset = args.train_dataset, list_data = args.list_train, 
+                                     phase='train', fold_i=i, n_folds=args.n_folds)
+        train_loaders.append(DataLoader(train_dataset, batch_size = args.batch_size, shuffle=True, num_workers=args.num_workers))
+        valid_dataset = SteelDataset(root_dataset = args.train_dataset, list_data = args.list_train, 
+                                     phase='valid', fold_i=i, n_folds=args.n_folds)
+        valid_loaders.append(DataLoader(valid_dataset, batch_size = args.batch_size, shuffle=True, num_workers=args.num_workers))
+        models.append(torchvision.models.resnet101(pretrained=True))
+        models[-1].fc=nn.Linear(models[-1].fc.in_features, num_classes)
+        models[-1] = models[-1].cuda()
+        optimizers.append(optim.Adam(models[-1].parameters(), lr=args.lr))
+        
+loss_fn = nn.CrossEntropyLoss()
 
-test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=args.num_workers)
-
-type_list = [1, 2, 3, 4]
-submission = pd.read_csv(args.list_test)
-
-# the function is from https://www.kaggle.com/paulorzp/rle-functions-run-lenght-encode-decode
-def mask2rle(img):
-    '''
-    img: numpy array, 1 - mask, 0 - background
-    Returns run length as string formated
-    '''
-    pixels= img.T.flatten()
-    pixels = np.concatenate([[0], pixels, [0]])
-    runs = np.where(pixels[1:] != pixels[:-1])[0] + 1
-    runs[1::2] -= runs[::2]
-    return ' '.join(str(x) for x in runs)
-
-def output2rle(mask_output, type=1):
-    '''change a certain type of model mask output to the submission type'''
-    
-    mask = np.where(mask_output==type, 1, 0)
-    return mask2rle(mask)
-    
-# the function is from https://www.kaggle.com/bibek777/heng-s-model-inference-kernel
-def post_process(probability, threshold, min_size):
-    '''Post processing of each predicted mask, components with lesser number of pixels
-    than `min_size` are ignored'''
-    mask = cv2.threshold(probability, threshold, 1, cv2.THRESH_BINARY)[1]
-    num_component, component = cv2.connectedComponents(mask.astype(np.uint8))
-    predictions = np.zeros((256, 1600), np.float32)
-    num = 0
-    for c in range(1, num_component):
-        p = (component == c)
-        if p.sum() > min_size:
-            predictions[p] = 1
-            num += 1
-    return predictions, num
-
-def test(data_loader):
-    model.eval()
-    for img, segm, img_id in tqdm(data_loader):
+def train(data_loader, model, optimizer):
+    model.train()
+    total_loss = 0
+    accumulation_steps = 32 // args.batch_size
+    optimizer.zero_grad()
+    for idx, (img, cls) in enumerate(tqdm(data_loader)):
         img = img.cuda()
-        output = model(img).cpu().detach().numpy()
-        print(output)
-        a=1/0
-        submission.loc[submission['ImageId_ClassId']==img_id[0]+'_'+str(type), 'EncodedPixels'] = rle
-    submission.to_csv(args.submission)
+        cls = cls.cuda()
+        outputs = model(img)
+        loss = loss_fn(outputs, cls)
+        (loss/accumulation_steps).backward()
+        clipping_value = 1.0
+        nn.utils.clip_grad_norm_(model.parameters(), clipping_value)
+        if (idx + 1 ) % accumulation_steps == 0:
+            optimizer.step()
+            optimizer.zero_grad()
+            with open(args.new_checkpoint_path+'logs.txt', 'a') as f:
+                print('idx:'+str(idx)+'  last_loss:'+str(loss), f)
+        total_loss += loss.item()
+        
+        # delete caches
+        del img, cls, outputs, loss
+        torch.cuda.empty_cache()
+            
+    return total_loss/len(data_loader)
 
-test(test_loader)
+def valid(data_loader, model):
+    model.eval()
+    num_correct = 0
+    accumulation_steps = 32 // args.batch_size
+    for idx, (img, cls) in enumerate(tqdm(data_loader)):
+        img = img.cuda()
+        cls = cls.cuda()
+        outputs = model(img)
+        
+        preds = outputs.argmax(dim=1)
+        num_correct += torch.eq(preds, cls).sum().float().item()
+        
+        # delete caches
+        del img, cls, outputs, preds
+        torch.cuda.empty_cache()
+            
+    return num_correct/len(data_loader)
+  
+for epoch in range(args.epoch_start, args.epoch_start+args.num_epoch):
+    start_time = time.time()
+    corrections_train = []
+    
+    for i in range(len(train_loaders)):
+        loss_train = train(train_loaders[i], models[i], optimizers[i])
+        if len(valid_loaders) > 0:
+            corrections_train.append(valid(valid_loaders[i], models[i]))
+        print('[TRAIN] Epoch: {}| Fold: {}| Loss: {}| Time: {}'.format(epoch, i, loss_train, time.time()-start_time))
+        state = {
+        "status": 'not used',
+        "epoch": epoch,
+        "arch": 'res101_' + str(i),
+        "state_dict": models[i].state_dict()
+        }
+        torch.save(state, '{}{}_checkpoint_{}.pth'.format(args.new_checkpoint_path, 'res101_' + str(i), epoch))
+    
+    if len(valid_loaders) > 0:
+        print('correction:'+str(sum(corrections_train)/len(corrections_train)))

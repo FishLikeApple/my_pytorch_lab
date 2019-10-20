8bfa80a3de3a7fa2741c3023b54f0a583db14621
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2019-10-20 13:59:09 +0800

    Create train.py

---
+++ segmentation/train.py
@@ -0,0 +1,85 @@
+import os
+import cv2
+import collections
+import time 
+import tqdm
+from PIL import Image
+from functools import partial
+train_on_gpu = True
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+%matplotlib inline
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score
+
+import torchvision
+import torchvision.transforms as transforms
+import torch
+from torch.utils.data import TensorDataset, DataLoader,Dataset
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch.utils.data.sampler import SubsetRandomSampler
+from torch.optim.lr_scheduler import StepLR, ReduceLROnPlateau, CosineAnnealingLR
+
+import albumentations as albu
+from albumentations import torch as AT
+
+from catalyst.data import Augmentor
+from catalyst.dl import utils
+from catalyst.data.reader import ImageReader, ScalarReader, ReaderCompose, LambdaReader
+from catalyst.dl.runner import SupervisedRunner
+from catalyst.contrib.models.segmentation import Unet
+from catalyst.dl.callbacks import DiceCallback, EarlyStoppingCallback, InferCallback, CheckpointCallback
+
+import segmentation_models_pytorch as smp
+
+from helper_functions_and_classes import *
+
+num_workers = 0
+bs = 16
+train_dataset = CloudDataset(df=train, datatype='train', img_ids=train_ids, transforms = get_training_augmentation(), preprocessing=get_preprocessing(preprocessing_fn))
+valid_dataset = CloudDataset(df=train, datatype='valid', img_ids=valid_ids, transforms = get_validation_augmentation(), preprocessing=get_preprocessing(preprocessing_fn))
+
+train_loader = DataLoader(train_dataset, batch_size=bs, shuffle=True, num_workers=num_workers)
+valid_loader = DataLoader(valid_dataset, batch_size=bs, shuffle=False, num_workers=num_workers)
+
+loaders = {
+    "train": train_loader,
+    "valid": valid_loader
+}
+
+num_epochs = 19
+logdir = "./logs/segmentation"
+
+# model, criterion, optimizer
+optimizer = torch.optim.Adam([
+    {'params': model.decoder.parameters(), 'lr': 1e-2}, 
+    {'params': model.encoder.parameters(), 'lr': 1e-3},  
+])
+scheduler = ReduceLROnPlateau(optimizer, factor=0.15, patience=2)
+criterion = smp.utils.losses.BCEDiceLoss(eps=1.)
+runner = SupervisedRunner()
+
+runner.train(
+    model=model,
+    criterion=criterion,
+    optimizer=optimizer,
+    scheduler=scheduler,
+    loaders=loaders,
+    callbacks=[DiceCallback(), EarlyStoppingCallback(patience=5, min_delta=0.001)],
+    logdir=logdir,
+    num_epochs=num_epochs,
+    verbose=True
+)
+
+utils.plot_metrics(
+    logdir=logdir, 
+    # specify which metrics we want to plot
+    metrics=["loss", "dice", 'lr', '_base/lr']
+)

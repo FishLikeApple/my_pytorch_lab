8b030c7e4776a276cf143de06bb736f61d541dd7
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2019-10-20 14:53:41 +0800

    Create args.py

---
+++ segmentation/args.py
@@ -0,0 +1,8 @@
+path = '../input/understanding_cloud_organization'
+num_workers = 0
+bs = 16
+num_epochs = 19
+logdir = "./logs/segmentation"
+ENCODER = 'resnet50'
+ENCODER_WEIGHTS = 'imagenet'
+DEVICE = 'cuda'

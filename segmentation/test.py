81a009a01e1d6c020e28f99f605adc910b6273b3
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2019-10-24 17:32:19 +0800

    Create test.py

---
+++ segmentation/test.py
@@ -0,0 +1,123 @@
+import os
+import cv2
+import collections
+import time 
+import tqdm
+from PIL import Image
+from functools import partial
+train_on_gpu = True
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score
+
+import torchvision
+import torchvision.transforms as transforms
+import torch
+from torch.utils.data import TensorDataset, DataLoader,Dataset
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch.utils.data.sampler import SubsetRandomSampler
+from torch.optim.lr_scheduler import StepLR, ReduceLROnPlateau, CosineAnnealingLR
+
+import albumentations as albu
+from albumentations import torch as AT
+
+from catalyst.data import Augmentor
+from catalyst.dl import utils
+from catalyst.data.reader import ImageReader, ScalarReader, ReaderCompose, LambdaReader
+from catalyst.dl.runner import SupervisedRunner
+from catalyst.contrib.models.segmentation import Unet
+from catalyst.dl.callbacks import DiceCallback, EarlyStoppingCallback, InferCallback, CheckpointCallback
+
+import segmentation_models_pytorch as smp
+
+encoded_pixels = []
+loaders = {"infer": valid_loader}
+runner.infer(
+    model=model,
+    loaders=loaders,
+    callbacks=[
+        CheckpointCallback(
+            resume=f"{logdir}/checkpoints/best.pth"),
+        InferCallback()
+    ],
+)
+valid_masks = []
+probabilities = np.zeros((2220, 350, 525))
+for i, (batch, output) in enumerate(tqdm.tqdm(zip(
+        valid_dataset, runner.callbacks[0].predictions["logits"]))):
+    image, mask = batch
+    for m in mask:
+        if m.shape != (350, 525):
+            m = cv2.resize(m, dsize=(525, 350), interpolation=cv2.INTER_LINEAR)
+        valid_masks.append(m)
+
+    for j, probability in enumerate(output):
+        if probability.shape != (350, 525):
+            probability = cv2.resize(probability, dsize=(525, 350), interpolation=cv2.INTER_LINEAR)
+        probabilities[i * 4 + j, :, :] = probability
+        
+class_params = {}
+for class_id in range(4):
+    print(class_id)
+    attempts = []
+    for t in range(0, 100, 5):
+        t /= 100
+        for ms in [0, 100, 1200, 5000, 10000]:
+            masks = []
+            for i in range(class_id, len(probabilities), 4):
+                probability = probabilities[i]
+                predict, num_predict = post_process(sigmoid(probability), t, ms)
+                masks.append(predict)
+
+            d = []
+            for i, j in zip(masks, valid_masks[class_id::4]):
+                if (i.sum() == 0) & (j.sum() == 0):
+                    d.append(1)
+                else:
+                    d.append(dice(i, j))
+
+            attempts.append((t, ms, np.mean(d)))
+
+    attempts_df = pd.DataFrame(attempts, columns=['threshold', 'size', 'dice'])
+
+
+    attempts_df = attempts_df.sort_values('dice', ascending=False)
+    print(attempts_df.head())
+    best_threshold = attempts_df['threshold'].values[0]
+    best_size = attempts_df['size'].values[0]
+    
+    class_params[class_id] = (best_threshold, best_size)
+    
+test_dataset = CloudDataset(df=sub, datatype='test', img_ids=test_ids, transforms = get_validation_augmentation(), preprocessing=get_preprocessing(preprocessing_fn))
+test_loader = DataLoader(test_dataset, batch_size=8, shuffle=False, num_workers=0)
+
+loaders = {"test": test_loader}
+
+encoded_pixels = []
+image_id = 0
+for i, test_batch in enumerate(tqdm.tqdm(loaders['test'])):
+    runner_out = runner.predict_batch({"features": test_batch[0].cuda()})['logits']
+    for i, batch in enumerate(runner_out):
+        for probability in batch:
+            
+            probability = probability.cpu().detach().numpy()
+            if probability.shape != (350, 525):
+                probability = cv2.resize(probability, dsize=(525, 350), interpolation=cv2.INTER_LINEAR)
+            predict, num_predict = post_process(sigmoid(probability), class_params[image_id % 4][0], class_params[image_id % 4][1])
+            if num_predict == 0:
+                encoded_pixels.append('')
+            else:
+                r = mask2rle(predict)
+                encoded_pixels.append(r)
+            image_id += 1
+            
+sub['EncodedPixels'] = encoded_pixels
+sub.to_csv('submission.csv', columns=['Image_Label', 'EncodedPixels'], index=False)
